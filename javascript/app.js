ee6bd4117868027a2a08c91218e8471cbd4430fe
mokmokahr <jbmok.workspace@gmail.com>
2022-10-06 22:17:43 +0900

    FEAT: Route and move the button to the middle

--- javascript/app.js
+++ javascript/app.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const app = express();
 
+app.use(express.static("views"));
+
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
 app.get('/', (req,res)=>{
-    res.render("index.html");
+    res.render("index");
 });
 
 app.get('/main',(req,res)=>{
-    res.render("main.ejs",{number:""});
+    res.render("main.ejs",{numbers: " "});
+});
+
+app.listen("3000",()=>{
+    console.log("server is running on the 3000 port...");
 });
